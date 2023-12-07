67c02e8f65219bd8b4acecd6a21b56d8dfa1ebf7
Veronika <Vasileva.s.veronika@gmail.com>
2023-12-07 20:30:09 +0300

    Добавила решение 2 задачи

---
+++ tasks 002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2
+
+void ChangeArray(int[,] array)
+{
+    for (int  i = 0; i < array.GetLength(0); i++)
+    {
+        for (int  j = 0; j < array.GetLength(1); j++)
+        {   
+            if ( i > 0 &&  i < array.GetLength(1))
+           {
+                int  temporary = array[i, j];
+                array[i, j] = array[i, j];
+                array[i, j] = temporary;
+            }
+            else
+            {
+                int  temporary = array[i, j];
+                array[i, j] = array[(array.GetLength(1) - 1), j];
+                array[(array.GetLength(1) - 1), j] = temporary;
+            }
+        }
+    }
+}
