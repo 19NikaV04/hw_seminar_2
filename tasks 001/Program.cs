eab0fe1602c9081a1c1b60c575251b129d68eafe
Veronika <Vasileva.s.veronika@gmail.com>
2023-12-07 20:21:55 +0300

    Добавила решение 1 задачи

---
+++ tasks 001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1
+
+double[,] ranArray = new double[m,n];
+Random rand = new Random();
+
+void mas(int m, int n)
+{
+     for (i = 0; i < m.GetLength; i++)
+    {
+        for (j = 0; j < n.GetLength; j++)
+       {
+           ranArray[i,j] = rand.NextDouble() * 1000;
+          Console.Write($«{ranArray[i,j]}»);
+       }
+      Console.WriteLine();
+    }
+}
+
+if (m2<1 || n2<1)
+{
+    Console.Write($«\n Позиции строк не могут быть отрицательными»);
+}
+else 
+{
+    if (m2 <= m+1 &&  n2 <= n+1)
+   Console.WriteLine($«\n Значение элемента равно: {ranArray[m2-1,n2-1]}»);
+}
+Console.WriteLine($«\n Такого элемента нет в массиве»);
